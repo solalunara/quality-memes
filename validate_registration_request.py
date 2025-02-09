c157e3e0dbee29e08c7bf10860790d62100d88ae
Solalunara <greenberg04@gmail.com>
2025-02-09 12:30:23 +0000

    issue string handling

--- validate_registration_request.py
+++ validate_registration_request.py
@@ -5,13 +5,15 @@ if __name__ == "__main__":
     request_plaintext = sys.argv[ 1 ];
     request = json.loads( request_plaintext );
     users_file = open( "users.json", "r" );
-    existing = json.loads( users_file.read() );
+    users_file_str = users_file.read();
+    existing = json.loads( users_file_str );
     users_file.close();
-    for user in existing.users:
-        if ( request.uname == user.uname ):
+    print( existing );
+    for user in existing[ "users" ]:
+        if ( request[ "uname" ] == user[ "uname" ] ):
             exit( 1 );
 
-    existing.users.append( request );
-    users_file_write = open( "users.json.new", "rw" );
+    existing[ "users" ].append( request );
+    users_file_write = open( "users.json.new", "w" );
     users_file_write.write( json.dumps( existing ) );
     users_file_write.close();
