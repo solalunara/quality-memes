2c6b048c6e584fe239ec7c3eb08c7d03c6c91b03
Solalunara <greenberg04@gmail.com>
2025-02-09 11:58:16 +0000

    user registration requests py file

---
+++ validate_registration_requests.py
@@ -0,0 +1,16 @@
+import sys
+import json
+
+if __name__ == "__main__":
+    request_plaintext = sys.argv[ 1 ];
+    request = json.loads( request_plaintext );
+    users_file = open( "users.json", "r" );
+    existing = json.loads( users_file.read() );
+    users_file.close();
+    for user in existing.users:
+        if ( request.uname == user.uname ):
+            exit( 1 );
+
+    existing.users.append( request );
+    users_file_write = open( "users.json", "rw" );
+    users_file_write.close();
